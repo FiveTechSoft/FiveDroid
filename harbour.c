bcc67b0a359ec1ace2cf40667bd91b23f9d8084f
FiveTech Software <antonio.fivetech@gmail.com>
2019-03-22 09:55:09 +0100

    Add files via upload

---
+++ harbour.c
@@ -0,0 +1,7 @@
+#include <jni.h>
+#include <hbapi.h>
+
+JNIEXPORT void JNICALL void Java_com_example_myapplication_MainActivity_vmInit( JNIEnv *env, jobject obj, jint i )
+{
+    // hb_vmInit( i );
+}
